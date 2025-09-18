63faeddd5dcd7be12ce9dbb06f88ef42e208b1e0
Developer9719 <justin.bat19@gmail.com>
2025-09-18 15:39:54 -0400

    Dynamically creates the webpage via script
    Creates 1 top section photo, a section below with 2 columns, a section below with 1 column and a footer section

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,5 +1,21 @@
 export function loadPage() {
     // Creates the basic structure of the webpage
     const content = document.getElementById("content");
-    content.innerHTML = `Test from JS loadPage() function. It works.`;
+    // content.innerHTML = `Test from JS loadPage() function. It works.`;
+
+    // Top Photo Section
+    content.appendChild(document.createElement("div")).id = "top-photo"; // Creating one top photo div
+
+    // Two Column Layout
+    content.appendChild(document.createElement("div")).className = "twoLayout";
+    const twoLayout = document.querySelector(".twoLayout");
+    
+    for (let i = 0; i < 2; i++) {
+        // Create a 2 column layout via css grid
+        twoLayout.appendChild(document.createElement("div")).className = "section";
+    }
+
+    content.appendChild(document.createElement("div")).id = "bottomSection"; // Creating one bottom section
+
+    content.appendChild(document.createElement("footer")).id = "footer"; // Creating one footer
 }
