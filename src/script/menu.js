1e1e0b0ccac1cdcb3a366b3ef3870d24154250c8
Developer9719 <justin.bat19@gmail.com>
2025-09-19 11:29:11 -0400

    Working on menu page

--- src/script/menu.js
+++ src/script/menu.js
@@ -1,5 +1,19 @@
 import "../css/menu.css";
 
 export function menu() {
-    
+    const contentArea = document.querySelector('.menu');
+    contentArea.appendChild(document.createElement('h2'));
+    contentArea.appendChild(document.createElement('p'));
+
+    const title = document.querySelector('h2');
+    const menuItemsDisplay = document.querySelector('p');
+
+    const sectionTitle = 'Menu';
+
+    title.textContent = sectionTitle;
+
+    const menuItems = ['Item 1', 'Item 2','Item 3'];
+    menuItems.forEach(element => {
+        menuItemsDisplay.innerHTML = `${element} <br/>`; // Displays only 'Item 3'
+    });
 }
