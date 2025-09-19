a6fe18989fc9bb3c1f62033a76af2c6f8db06c69
Developer9719 <justin.bat19@gmail.com>
2025-09-19 00:59:14 -0400

    Top area image added
    Image not displaying yet

--- src/script/topViewport.js
+++ src/script/topViewport.js
@@ -1,5 +1,7 @@
 import '../css/topViewport.css';
 
 export function topArea() {
-    
+    const topArea = document.querySelector('.topArea');
+
+    topArea.innerHTML = '<img src="<img src="https://www.daysoftheyear.com/cdn-cgi/image/dpr=1%2Cf=auto%2Cfit=cover%2Ch=675%2Cq=85%2Cw=1200/wp-content/uploads/national-fast-food-day.jpg" alt="National Fast Food Day (November 16th) | Days Of The Year"/>" class="topAreaImg">'
 }
